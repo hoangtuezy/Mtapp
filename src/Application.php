4329748a3d23b2b7fca24c8223234924305ee213
vohoangtu0409 <9281384+hoangtucoding@users.noreply.github.com>
2020-05-07 17:05:17 +0700

    update

--- src/Application.php
+++ src/Application.php
@@ -32,31 +32,7 @@ class Application{
 		$this->app = array();
 		// $this->template = new Blade('views','cache');
 	}
-	public function add_filter($key,$filter){
-		array_push($this->filters, array($key,$filter));
-	}
-	public function filters(){
-		return $this->filters;
-	}
-	public function setView($viewFolder,$cache = null){
-		$this->template = new AbstractView($viewFolder,$cache);
-	}
-	public function template() : AbstractView{
-		return $this->template;
-	}
-	public function request() : Request{
-		return $this->request;
-	}
-	public function response() : Response{
-		return $this->response;
-	}
-	public function module() {
-		return $this->module;
-	}
-	public function session() {
-		return $this->session;
-	}
-	public function init($config){
+		public function init($config){
 		$this->config = $config;
 		$this->request = new Request([
 			'_get'=>$_GET,
@@ -71,55 +47,11 @@ class Application{
 		}
 
 	}
-	public function register($apps){
-		foreach($apps as $key => $item){
-			$key = md5($key);
-			$this->app['name'][] = $key;
-			$this->app[$key]['name'] = $key;
-			$this->app[$key]['dir'] = $item;
-		}
-		
+	public function setView($viewFolder,$cache = null){
+		$this->template = new AbstractView($viewFolder,$cache);
+	}
+	public function template() : AbstractView{
+		return $this->template;
 	}
 
-	public function setup(){
-		foreach($this->app['name'] as $name){
-			$dir = $this->app[$name]['dir'];
-			$basename_dir = basename($dir);
-		}
-	}
-
-	public function dump($arr, $exit=1){
-		echo "<pre>";
-		var_dump($arr);
-		echo "<pre>";
-		if($exit)exit();
-	}
-	public function set_database($data){
-		if(empty($this->database)){
-			$this->database = new Database();
-		}
-		$this->database->set_database($data);
-	}
-	public function handle(){
-		$this->dump($this);
-		if(in_array($this->request->getRequestTarget(), $this->app['name'])){
-			$this->module = $this->request->getRequestTarget();
-		}else{
-			$request_uri = explode('/', $this->request->getRequestTarget());
-			$this->module = $request_uri[1];
-		}
-		include $this->app[$this->module]['dir']."/index.php";
-
-	}
-	
-	public function run($before = [],$after = [],$calla=null){
-		if(!empty($before)){
-			foreach ($before as $key => $item){
-				(new $item($this))->process($this);
-			}
-		}
-		if(is_callable($calla)){
-			$calla($this);
-		}
-	}
 }
