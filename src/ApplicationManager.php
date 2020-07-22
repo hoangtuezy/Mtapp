56513c103acaa44931884a60c1f07bd47b5789f3
hoangtuezy <61352563+hoangtuezy@users.noreply.github.com>
2020-07-23 01:26:27 +0700

    up

--- src/ApplicationManager.php
+++ src/ApplicationManager.php
@@ -1,12 +1,22 @@
 <?php
 namespace Vht\Src;
 
+use Illuminate\Container\Container as Container;
+use Vht\Src\Application;
+
 class ApplicationManager{
-	protected $application_manager[];
-	public function __construct(){
-		$this->application_manager = new \Illuminate\Container\Container;
+	private $application_manager;
+	function __construct(){
+		$this->application_manager = new Container;
 	}
-	public function register(\Vht\Src\Application $application){
-		$this->application_manager->instance("app_".strtolower($application->getName()),$application);
+
+	function register($app_name,$cl = null){
+		if(is_null($cl))
+			return $this->application_manager->instance($app_name,new Application);
+		else
+			return $this->application_manager->singleton($app_name,$cl);
+	}
+	function get($app_name) : \Vht\Src\Application{
+		return $this->application_manager->$app_name;
 	}
 }
